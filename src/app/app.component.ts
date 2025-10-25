8b534cbb3d0a109c303d35ee4cb69023542085ca
iyed Mdimegh <iyedmdimegh@gmail.com>
2025-10-25 10:17:11 +0100

    some updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { RouterOutlet } from '@angular/router';
+import { RainbowDirective } from './directives/rainbow.directive';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
     standalone: true,
-    imports: [NavbarComponent, RouterOutlet]
+    imports: [NavbarComponent, RouterOutlet, RainbowDirective]
 })
 export class AppComponent {
   title = 'Starting Advanced Topics';
--- src/app/directives/rainbow.directive.ts
+++ src/app/directives/rainbow.directive.ts
@@ -1,8 +1,8 @@
 import { Directive, ElementRef, HostBinding, HostListener, signal } from '@angular/core';
 
 @Directive({
-  selector: '[appRainbow]',
-  standalone: true
+  selector: 'input[appRainbow][type=text],texetarea',
+  standalone: true, 
 })
 export class RainbowDirective {
   private colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
@@ -14,18 +14,11 @@ export class RainbowDirective {
     return this.colorSignal();
   }
 
-  constructor(private el: ElementRef) {
-    if (this.el.nativeElement.tagName !== 'INPUT') {
-      return;
-    }
-  }
-
   @HostListener('keyup')
   onKeyUp() {
     // Only work on input elements
-    if (this.el.nativeElement.tagName === 'INPUT') {
+
       const randomColor = this.colors[Math.floor(Math.random() * this.colors.length)];
       this.colorSignal.set(randomColor);
-    }
   }
 }
