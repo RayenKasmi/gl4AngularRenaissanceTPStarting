aeb56b6c157486c105b5d3d6ed4227e61bc91cb2
iyed Mdimegh <iyedmdimegh@gmail.com>
2025-10-25 03:15:48 +0100

    signal exercice

--- src/app/cv/details-cv/details-cv.component.ts
+++ src/app/cv/details-cv/details-cv.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, signal } from '@angular/core';
 import { Cv } from '../model/cv';
 import { CvService } from '../services/cv.service';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -22,7 +22,7 @@ export class DetailsCvComponent implements OnInit {
   private toastr = inject(ToastrService);
   authService = inject(AuthService);
 
-  cv: Cv | null = null;
+  cv = signal<Cv | null>(null);
 
   /** Inserted by Angular inject() migration for backwards compatibility */
   constructor(...args: unknown[]);
@@ -31,13 +31,13 @@ export class DetailsCvComponent implements OnInit {
   ngOnInit() {
     const id = this.activatedRoute.snapshot.params['id'];
     this.cvService.getCvById(+id).subscribe({
-        next: (cv) => {
-          this.cv = cv;
-        },
-        error: (e) => {
-          this.router.navigate([APP_ROUTES.cv]);
-        },
-      });
+      next: (cv) => {
+        this.cv.set(cv);
+      },
+      error: () => {
+        this.router.navigate([APP_ROUTES.cv]);
+      },
+    });
   }
   deleteCv(cv: Cv) {
     this.cvService.deleteCvById(cv.id).subscribe({
