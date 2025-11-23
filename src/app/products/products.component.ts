b236b3247d86d62eb26a51b4b06ce648e4ed50f9
RayenKasmi <rayenekasmi0@gmail.com>
2025-11-23 19:29:16 +0100

    feat: added the product loading behaviour using rxjs and subjects

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -20,7 +20,20 @@ export class ProductsComponent {
   /* Todo : Faire le nécessaire pour créer le flux des produits à afficher */
   /* Tips : vous pouvez voir les différents imports non utilisés et vous en inspirer */
   products$!: Observable<Product[]>;
-  constructor() {}
+  private load$ = new BehaviorSubject<number>(0) ;
+  private limit = 12;
 
-  
+  constructor(private productService: ProductService) {
+    this.products$ = this.load$.pipe(
+      concatMap((value) => this.productService.getProducts({limit: this.limit, skip: value*this.limit})),
+      takeWhile((value) => value.products.length > 0,true),
+      map((value) => value.products),
+      scan((acc , products) => [...acc, ...products]),
+    )
+  }
+
+  loadMore(){
+    this.load$.next(this.load$.value + 1);
+  }
+
 }
